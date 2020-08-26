395bcef406555d761de412a79cd7fcd9276d0454
Laureni Wilkinson <las348@nyu.edu>
2020-08-25 22:05:16 -0400

    add child

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,23 +4,35 @@ import ProfileCard from "../components/ProfileCard/profilecard";
 import UserContext from "../utils/userContext";
 
 function Profile() {
+
   const { userState, setUserState } = React.useContext(UserContext);
   const { user } = userState;
 
   const [profileState, setProfileState] = useState({
-    displayName: "",
+    displayName: user.displayName,
     address: {
-      city: "",
-      state: "",
-      zip: ""
+      city: (user && user.address && user.address.city) ? user.address.city : "",
+      state: (user && user.address && user.address.state) ? user.address.state : "",
+      zip: (user && user.address && user.address.zip) ? user.address.zip : "",
     }
   });
+
+  const [childState, setChildState] = useState({
+    firstName: (user && user.children && user.children[0].firstName) ? user.children[0].firstName : "",
+    lastName: (user && user.children && user.children[0].lastName) ? user.children[0].lastName : "",
+    age: (user && user.children && user.children[0].age) ? user.children[0].age : "",
+    activities: (user && user.children && user.children[0].activities) ? user.children[0].activities : "",
+  });
+
 
   function handleEdit(event) {
     const { name, value } = event.target;
     console.log("handleEdit clicked");
-    
-    setProfileState({ ...profileState, [name]: value })
+
+    const address = { ...profileState.address, [name]: value }
+    const newObj = { ...profileState, address }
+
+    setProfileState(newObj)
   }
 
 
@@ -36,52 +48,70 @@ function Profile() {
         zip: profileState.address.zip
       }
     }
+    console.log(profileUpdate);
 
     API.updateUser(profileUpdate, user.uid)
       .then((res) => {
         console.log(res);
-        setUserState({ ...profileState, user: res.data })
+        setUserState({ ...userState, user: res.data })
       })
-      // .then(window.location.reload(false))
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  //Child functions
+  function handleAddChild(event) {
+    const { name, value } = event.target;
+    console.log("handleAddChild clicked");
+
+    setChildState({ ...childState, [name]: value })
+  }
+
+  function addChild(event) {
+    event.preventDefault();
+
+    let childInfo = {
+      firstName: childState.firstName,
+      lastName: childState.lastName,
+      age: childState.age,
+      activities: childState.activities
+    }
+    console.log(childInfo);
+
+    API.addChild(childInfo, user.uid)
+      .then((res) => {
+        console.log(res);
+        setChildState({ ...childState, child: res.data })
+      })
       .catch(error => {
         console.log(error)
       })
   }
 
 
-  // handleDeleteUser = event => {
-  //   event.preventDefault();
-
-  //   const userID = event.target.getAttribute('data-id')
-
-  //   const newState = { ...this.state }
-
-  //   newState.results = this.state.results.filter(user => user._id !== userID)
-  //   // Filters out any user with the matching target id
-
-  //   API.deleteUser(userID).then(
-  //     (response) => {
-  //       this.setState(newState)
-  //       console.log(response);
-  //     }
-  //   ).catch(
-  //     (err) => {
-  //       console.log(err);
-  //     }
-  //   );
-  // }
+  function deleteChild(childId) {
+    API.removeChild(childId)
+      .then(alert("child deleted"))
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   return (
     <ProfileCard
-      user={user}
       handleUpdateUser={handleUpdateUser}
       handleEdit={handleEdit}
       profileState={profileState}
-    // deleteUser={handleDeleteUser}
+      addChild={addChild}
+      childState={childState}
+      handleAddChild={handleAddChild}
+      deleteChild={deleteChild}
     />
   )
 }
 
-
 export default Profile;
+
+
 

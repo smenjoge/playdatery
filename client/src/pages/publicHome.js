6ee11cec133dc5b75f123087dca781153546eb4f
Sandesh Menjoge <smenjoge@gmail.com>
2020-09-03 23:37:20 -0400

    update

--- client/src/pages/publicHome.js
+++ client/src/pages/publicHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Testimonials from "../components/Testimonials/testimonials";
-import Carousel from '../components/Carousel/carousel';
+import Carousel from "../components/Carousel/carousel";
 
 function PublicHome() {
 
